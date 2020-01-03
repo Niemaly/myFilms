cf8e4463ebae217d16edfd5b6fd4ca0857e866ad
Niemaly <jac3k2@gmail.com>
2020-01-03 15:52:54 +0100

    Created Init class to init all films and users.

---
+++ src/main/java/com/Szumski/myFilms/configuration/Init.java
@@ -0,0 +1,105 @@
+package com.Szumski.myFilms.configuration;
+
+import com.Szumski.myFilms.model.MovieModel;
+import com.Szumski.myFilms.model.User;
+import com.Szumski.myFilms.model.UserMovie;
+import com.Szumski.myFilms.repository.MovieRepository;
+import com.Szumski.myFilms.service.UserMoviesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class Init {
+
+    private final WebSecurityConfig webSecurityConfig;
+    private UserMoviesService userMoviesService;
+    private MovieRepository movieRepository;
+
+    @Autowired
+    public Init(WebSecurityConfig webSecurityConfig, UserMoviesService userMoviesService, MovieRepository movieRepository) {
+        this.userMoviesService = userMoviesService;
+        this.webSecurityConfig = webSecurityConfig;
+        this.movieRepository = movieRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    private void setSomeUsers() {
+        User user1 = new User("user", webSecurityConfig.passwordEncoder().encode("user"), "USER");
+        User user2 = new User("u", webSecurityConfig.passwordEncoder().encode("user"), "USER");
+        User user3 = new User("useruser", webSecurityConfig.passwordEncoder().encode("user"), "USER");
+
+        System.out.println(user1.getUsername());
+        System.out.println(user2.getUsername());
+        System.out.println(user3.getUsername());
+
+        userMoviesService.getUserRepository().deleteAll();
+
+        userMoviesService.setUser(user1);
+        userMoviesService.createNewUser(user1);
+
+        userMoviesService.setUser(user2);
+        userMoviesService.createNewUser(user2);
+
+        userMoviesService.setUser(user3);
+        userMoviesService.createNewUser(user3);
+
+
+        List<UserMovie> userMoviesList1 = userMoviesService.getAllMovies(user1.getIdFilmList());
+        UserMovie userFilm = new UserMovie(111L);
+        UserMovie userFilm2 = new UserMovie(222L);
+
+        userFilm2.setRating(7.3);
+        userFilm.setRating(5.2);
+        userFilm.setStatus(true);
+        userFilm.setComment("this was awesome film man");
+        userMoviesList1.add(userFilm);
+        userMoviesList1.add(userFilm2);
+        userMoviesService.saveMovieToUser(userFilm, user1);
+        userMoviesService.saveMovieToUser(userFilm2, user1);
+
+        System.out.println(userMoviesService.getAllMovies(user1.getIdFilmList()));
+        System.out.println(userMoviesService.getAllMovies(user2.getIdFilmList()));
+
+        movieRepository.deleteAll();
+
+
+        movieRepository.saveAll(mockMovies());
+
+        movieRepository.findAll().stream().forEach(element -> {
+            System.out.println(element.toString());
+        });
+
+    }
+
+    private List<MovieModel> mockMovies() {
+        List<MovieModel> list = new ArrayList<MovieModel>();
+
+        MovieModel movieModel = new MovieModel();
+        movieModel.setTitle("Berlin: Die Sinfonie der Großstadt");
+        movieModel.setRating(7.8);
+        movieModel.setSrc("/ePGMsItIMHSOvZkCJ6rnl4v64Sq.jpg");
+        movieModel.setId(222L);
+        movieModel.setGenreIdList(new ArrayList<Integer>());
+        movieModel.setDateOfRelease("1927-09-23");
+
+
+        MovieModel movieModel2 = new MovieModel();
+        movieModel2.setTitle("Scarface");
+        movieModel2.setRating(8.1);
+        movieModel2.setSrc("/ePGMsItIMHSOvZkCJ6rnl4v64Sq.jpg");
+        movieModel2.setId(111L);
+        movieModel2.setGenreIdList(new ArrayList<Integer>());
+        movieModel2.setDateOfRelease("1983-12-08");
+
+        list.add(movieModel);
+        list.add(movieModel2);
+
+        return list;
+
+    }
+}
--- src/main/java/com/Szumski/myFilms/configuration/WebSecurityConfig.java
+++ src/main/java/com/Szumski/myFilms/configuration/WebSecurityConfig.java
@@ -1,16 +1,11 @@
 package com.Szumski.myFilms.configuration;
 
-import com.Szumski.myFilms.model.MovieModel;
-import com.Szumski.myFilms.model.User;
-import com.Szumski.myFilms.model.UserMovie;
 import com.Szumski.myFilms.repository.MovieRepository;
 import com.Szumski.myFilms.service.UserDetailsServiceImplementation;
 import com.Szumski.myFilms.service.UserMoviesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.EventListener;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,9 +16,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 
 @Configuration
@@ -77,85 +70,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     RestTemplate getRestTemplate(){
-        return  new RestTemplate();
-    }
-
-
-    @EventListener(ApplicationReadyEvent.class)
-    public void setSomeUsers(){
-        User user1 = new User("user", passwordEncoder().encode("user"), "USER");
-        User user2 = new User("u", passwordEncoder().encode("user"), "USER");
-        User user3 = new User("useruser", passwordEncoder().encode("user"), "USER");
-
-        System.out.println(user1.getUsername());
-        System.out.println(user2.getUsername());
-        System.out.println(user3.getUsername());
-
-        userMoviesService.getUserRepository().deleteAll();
-
-        userMoviesService.setUser(user1);
-        userMoviesService.createNewUser(user1);
-
-        userMoviesService.setUser(user2);
-        userMoviesService.createNewUser(user2);
-
-        userMoviesService.setUser(user3);
-        userMoviesService.createNewUser(user3);
-
-
-        List<UserMovie> userMoviesList1 = userMoviesService.getAllMovies(user1.getIdFilmList());
-        UserMovie userFilm = new UserMovie(111L);
-        UserMovie userFilm2 = new UserMovie(222L);
-
-        userFilm2.setRating(7.3);
-        userFilm.setRating(5.2);
-        userFilm.setStatus(true);
-        userFilm.setComment("this was awesome film man");
-        userMoviesList1.add(userFilm);
-        userMoviesList1.add(userFilm2);
-        userMoviesService.saveMovieToUser(userFilm, user1);
-        userMoviesService.saveMovieToUser(userFilm2, user1);
-
-        System.out.println(userMoviesService.getAllMovies(user1.getIdFilmList()));
-        System.out.println(userMoviesService.getAllMovies(user2.getIdFilmList()));
-
-        movieRepository.deleteAll();
-
-
-
-        movieRepository.saveAll(mockMovies());
-
-     movieRepository.findAll().stream().forEach(element->{
-     System.out.println(element.toString());
- });
-
-    }
-
-    private List<MovieModel> mockMovies(){
-        List<MovieModel> list = new ArrayList<>();
-
-        MovieModel movieModel = new MovieModel();
-        movieModel.setTitle("Berlin: Die Sinfonie der Großstadt");
-        movieModel.setRating(7.8);
-        movieModel.setSrc("/ePGMsItIMHSOvZkCJ6rnl4v64Sq.jpg");
-        movieModel.setId(222L);
-        movieModel.setGenreIdList(new ArrayList<>());
-        movieModel.setDateOfRelease("1927-09-23");
-
-
-        MovieModel movieModel2 = new MovieModel();
-        movieModel2.setTitle("Scarface");
-        movieModel2.setRating(8.1);
-        movieModel2.setSrc("/ePGMsItIMHSOvZkCJ6rnl4v64Sq.jpg");
-        movieModel2.setId(111L);
-        movieModel2.setGenreIdList(new ArrayList<>());
-        movieModel2.setDateOfRelease("1983-12-08");
-
-        list.add(movieModel);
-        list.add(movieModel2);
-
-        return list;
-
+        return new RestTemplate();
     }
 
 
